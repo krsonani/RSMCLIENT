0058dab5c56c5713a6701068e19b8a94b0aef90d
Timy A. Sam <timy4christ@gmail.com>
2023-05-26 21:59:43 +0530

    added token in requests

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Order } from './order';
 
@@ -7,12 +7,17 @@ import { Order } from './order';
 })
 export class CartService {
 
-  url:string = "http://localhost:8081"
-  
-  constructor(private http:HttpClient) { }
+  url: string = "http://localhost:8081"
 
-  addOrder(order: Order){
-    return this.http.post(this.url+"/addOrder",order);
+  constructor(private http: HttpClient) { }
+
+  addOrder(order: Order) {
+    return this.http.post(this.url + "/addOrder", order, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `${sessionStorage.getItem("jwtToken")}`
+      })
+    });
   }
 
 }
--- src/app/customerupdateprofile/customerupdateprofile.service.ts
+++ src/app/customerupdateprofile/customerupdateprofile.service.ts
@@ -7,25 +7,30 @@ import { Updateprofile } from './updateprofile';
 })
 export class CustomerupdateprofileService {
 
-  url:string = "http://localhost:8081"
-  constructor(private http:HttpClient) { }
+  url: string = "http://localhost:8081"
+  constructor(private http: HttpClient) { }
 
   getCustomer() {
-    return this.http.get<any>(this.url+"/getSession",{
-      headers:new HttpHeaders({
+    return this.http.get<any>(this.url + "/getSession", {
+      headers: new HttpHeaders({
         'Content-Type': 'application/json',
         'Authorization': `${sessionStorage.getItem("jwtToken")}`
       })
     })
   }
 
-  updateCustomerProfile(payload: Updateprofile){
-    return this.http.put(this.url + '/updateUser',payload);
+  updateCustomerProfile(payload: Updateprofile) {
+    return this.http.put(this.url + '/updateUser', payload, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `${sessionStorage.getItem("jwtToken")}`
+      })
+    })
   }
 
-  
-
-  
-
-
 }
+
+
+
+
+
