f596b8dabb1bbb341011fbddc18007ab13cf3348
Timy A. Sam <timy4christ@gmail.com>
2023-05-27 17:14:52 +0530

    order history updated

--- src/app/daywiseorder/daywiseorder.component.ts
+++ src/app/daywiseorder/daywiseorder.component.ts
@@ -9,42 +9,82 @@ import { DaywiseorderService } from './daywiseorder.service';
 export class DaywiseorderComponent implements OnInit {
 
   todaysOrders: any;
-  roleType: String = '';
-  userId: String = '';
+  roleType: string = '';
+  userId: string = '';
   now: string = '';
+  currentOrdertotalPrice: number = 0;
+  pastOrdertotalPrice: number = 0;
+
   constructor(private service: DaywiseorderService) { }
 
   ngOnInit(): void {
 
- 
-   // Assign here the roletype and userId thats all,for Now I have Hardcoded this values for testing purpose...
-    this.roleType='CUSTOMER';
-    this.userId='2';
-  
-    if (this.roleType =='ADMIN') {
-      this.service.showDayWiseOrders().subscribe({
-        next: (res) => {
-          console.log(res);
-          this.todaysOrders = res;
-        }, error: (err) => {
-          console.log(err);
-        }
-
-      })
-
-      this.now = new Date().toLocaleDateString();
-    }
-    else {
-      this.service.getOrderByUserId(this.userId).subscribe({
-        next: (res) => {
-          console.log(res);
-          this.todaysOrders = res;
-        }, error: (err) => {
-          console.log(err);
-        }
-      })
-
-    }
+    // Assign here the roletype and userId thats all,for Now I have Hardcoded this values for testing purpose...
+    this.getOrderHistory();
+    // console.log(todaysOrders);
+    
   }
+
+  billPayed() {
+    // console.log("chal rha hai");
+    this.service.updateBillGenerationStatus(this.userId).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.getOrderHistory();
+        this.currentOrdertotalPrice=0;
+      }, error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
+  getOrderHistory() {
+    this.service.getCustomer().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.roleType = res.role.type;
+        this.userId = res.uids;
+        // console.log(this.roleType);
+        // console.log(this.userId);
+
+        if (this.roleType == 'MANAGER') {
+          this.service.showDayWiseOrders().subscribe({
+            next: (res) => {
+              console.log(res);
+              this.todaysOrders = res;
+              for (let item of this.todaysOrders) {
+                if (item.billGenerated == true) {
+                  this.pastOrdertotalPrice += item.totalPrice;
+                }
+              }
+            }, error: (err) => {
+              console.log(err);
+            }
+
+          })
+
+          this.now = new Date().toLocaleDateString();
+        }
+        else {
+          this.service.getOrderByUserId(this.userId).subscribe({
+            next: (res) => {
+              console.log(res);
+              this.todaysOrders = res;
+              for (let item of this.todaysOrders) {
+                if (item.billGenerated == false) {
+                  this.currentOrdertotalPrice += item.totalPrice;
+                }
+              }
+            }, error: (err) => {
+              console.log(err);
+            }
+          })
+
+        }
+
+      }
+    })
+  }
+
 
 }
